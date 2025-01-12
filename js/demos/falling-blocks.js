9b9d399f4aff292fdcd4a8dafa653a6eb5231434
Barry G Becker <barrybecker4@gmail.com>
2025-01-12 08:21:41 -0800

    Put the demos in tabs in single file
    Also have pause/resume functionality so they don't all run at once.

--- js/demos/falling-blocks.js
+++ js/demos/falling-blocks.js
@@ -1,56 +1,72 @@
 //"use strict";
 import { dt, particles, advance, add_rnd_square } from '../mls-mpm.js';
 
-const CANVAS_SIZE = 600;
-const MARGIN = 48;
-const renderEvery = 5;
-const enableTiming = false;
+export function initFallingBlocks(canvas, size) {
 
-document.documentElement.style.setProperty('--canvas-size', `${CANVAS_SIZE}px`);
-const canvas = document.getElementById('canvas');
-canvas.width = CANVAS_SIZE;
-canvas.height = CANVAS_SIZE;
+  let isRunning = true;
+  const MARGIN = 48;
+  const renderEvery = 5;
+  const enableTiming = false;
 
-const context = canvas.getContext('2d', { alpha: false });
-let iter = 0;
+  document.documentElement.style.setProperty('--canvas-size', `${size}px`);
+  canvas.width = size;
+  canvas.height = size;
 
-function display() {
-    context.fillStyle = '#000000';
-    context.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  const context = canvas.getContext('2d', { alpha: false });
+  let iter = 0;
 
-    context.lineWidth = 0.5;
-    context.strokeStyle = '#CCCCCC';
-    context.strokeRect(24, 24, CANVAS_SIZE - MARGIN, CANVAS_SIZE - MARGIN);
+  function display() {
+      context.fillStyle = '#000000';
+      context.fillRect(0, 0, size, size);
 
-    for(let p of particles) {
-        context.fillStyle = `#${p.c.toString(16)}`;
-        context.fillRect(CANVAS_SIZE * p.x[0] - 1, CANVAS_SIZE - CANVAS_SIZE * p.x[1] - 1, 2, 2);
+      context.lineWidth = 0.5;
+      context.strokeStyle = '#CCCCCC';
+      context.strokeRect(24, 24, size - MARGIN, size - MARGIN);
+
+      for (let p of particles) {
+          context.fillStyle = `#${p.c.toString(16)}`;
+          context.fillRect(size * p.x[0] - 1, size - size * p.x[1] - 1, 2, 2);
+      }
+  }
+
+  function step() {
+      if (!isRunning) return;
+
+      requestAnimationFrame(step);
+      iter++;
+
+      const mustRender = iter % renderEvery === 0;
+      const mustTime = mustRender && enableTiming;
+
+      // Advance simulation
+      mustTime && console.time('advance');
+      advance(dt);
+      mustTime && console.timeEnd('advance');
+
+      if (mustRender) {
+          mustTime && console.time('display');
+          display();
+          mustTime && console.timeEnd('display');
+      }
+  }
+
+  enableTiming && console.time('setup');
+  add_rnd_square([0.55,0.45], 0xED553B);
+  add_rnd_square([0.45,0.65], 0xF2B134);
+  add_rnd_square([0.55,0.85], 0x168587);
+  enableTiming && console.timeEnd('setup');
+
+  step();
+
+  return {
+    pause: () => {
+      isRunning = false;
+    },
+    resume: () => {
+      if (!isRunning) {
+        isRunning = true;
+        step();
+      }
     }
+  };
 }
-
-function step() {
-    requestAnimationFrame(step);
-    iter++;
-
-    const mustRender = iter % renderEvery === 0;
-    const mustTime = mustRender && enableTiming;
-
-    // Advance simulation
-    mustTime && console.time('advance');
-    advance(dt);
-    mustTime && console.timeEnd('advance');
-
-    if(mustRender) {
-        mustTime && console.time('display');
-        display();
-        mustTime && console.timeEnd('display');
-    }
-}
-
-enableTiming && console.time('setup');
-add_rnd_square([0.55,0.45], 0xED553B);
-add_rnd_square([0.45,0.65], 0xF2B134);
-add_rnd_square([0.55,0.85], 0x168587);
-enableTiming && console.timeEnd('setup');
-
-step();
