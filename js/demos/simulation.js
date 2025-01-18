8752d21d2fe9bb9099550df722f321516a5d6d4d
Barry G Becker <barrybecker4@gmail.com>
2025-01-18 07:51:07 -0800

    simulation constants

--- js/demos/simulation.js
+++ js/demos/simulation.js
@@ -1,11 +1,16 @@
+const MARGIN = 48;
+const BORDER_COLOR = '#DDDDDD';
+const BG_COLOR = '#000000';
+const RENDER_EVERY = 5;
+const ENABLE_TIMING = false;
+const PARTICLE_SIZE = 2;
+const BORDER_WIDTH = 1;
+
 
 export function createSimulation(initializeSimulation, advanceSimulation, particles) {
 
     return function init(canvas, size) {
         let isRunning = true;
-        const MARGIN = 48;
-        const renderEvery = 5;
-        const enableTiming = false;
 
         document.documentElement.style.setProperty('--canvas-size', `${size}px`);
         canvas.width = size;
@@ -15,16 +20,24 @@ export function createSimulation(initializeSimulation, advanceSimulation, partic
         let iter = 0;
 
         function display() {
-            context.fillStyle = '#000000';
+            renderFrame();
+            renderParticles();
+        }
+
+        function renderFrame() {
+            context.fillStyle = BG_COLOR;
             context.fillRect(0, 0, size, size);
 
             context.lineWidth = 1;
-            context.strokeStyle = '#DDDDDD';
-            context.strokeRect(24, 24, size - MARGIN, size - MARGIN);
+            context.strokeStyle = BORDER_COLOR;
+            context.strokeRect(MARGIN / 2, MARGIN / 2, size - MARGIN, size - MARGIN);
+        }
 
+        function renderParticles() {
+            const halfSize = PARTICLE_SIZE / 2;
             for (let p of particles) {
                 context.fillStyle = `#${p.c.toString(16)}`;
-                context.fillRect(size * p.x[0] - 1, size - size * p.x[1] - 1, 2, 2);
+                context.fillRect(size * p.x[0] - halfSize, size - size * p.x[1] - halfSize, PARTICLE_SIZE, PARTICLE_SIZE);
             }
         }
 
@@ -33,8 +46,8 @@ export function createSimulation(initializeSimulation, advanceSimulation, partic
             requestAnimationFrame(step);
             iter++;
 
-            const mustRender = iter % renderEvery === 0;
-            const mustTime = mustRender && enableTiming;
+            const mustRender = iter % RENDER_EVERY === 0;
+            const mustTime = mustRender && ENABLE_TIMING;
 
             mustTime && console.time('advance');
             advanceSimulation(iter);
@@ -47,9 +60,9 @@ export function createSimulation(initializeSimulation, advanceSimulation, partic
             }
         }
 
-        enableTiming && console.time('setup');
+        ENABLE_TIMING && console.time('setup');
         initializeSimulation();
-        enableTiming && console.timeEnd('setup');
+        ENABLE_TIMING && console.timeEnd('setup');
 
         step();
 
