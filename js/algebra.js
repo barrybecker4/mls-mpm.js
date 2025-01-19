a3486e998dc46877e0e53a9dcebc42e00724b6a0
Barry G Becker <barrybecker4@gmail.com>
2025-01-19 04:47:24 -0800

    use arrays directly

--- js/algebra.js
+++ js/algebra.js
@@ -1,10 +1,10 @@
-export function add2D(a, b) {return [a[0] + b[0], a[1] + b[1]]}
-export function sca2D(a, t) {return [a[0] * t, a[1] * t]}
-export function sub2D(a, b) {return [a[0] - b[0], a[1]-b[1]]}
-export function add3D(a, b) {return [a[0] + b[0], a[1] + b[1], a[2] + b[2]]}
-export function sca3D(a, t) {return [a[0] * t, a[1] * t, a[2] * t]}
-export function determinant(a) {return a[0] * a[3] - a[1] * a[2]}
-export function transposed(a) {return [a[0], a[2], a[1], a[3]]}
+export function add2D(a, b) { return [a[0] + b[0], a[1] + b[1]] }
+export function sca2D(a, t) { return [a[0] * t, a[1] * t]}
+export function sub2D(a, b) { return [a[0] - b[0], a[1]-b[1]]}
+export function add3D(a, b) { return [a[0] + b[0], a[1] + b[1], a[2] + b[2]] }
+export function sca3D(a, t) { return [a[0] * t, a[1] * t, a[2] * t] }
+export function determinant(a) { return a[0] * a[3] - a[1] * a[2] }
+export function transposed(a) { return [a[0], a[2], a[1], a[3]] }
 
 export function mulMat(a, b) {
     return [
@@ -43,11 +43,7 @@ export function polar_decomp(m) {
     const scale = 1.0 / Math.sqrt(x * x + y * y);
     const c = x * scale;
     const s = y * scale;
-    const R = [];
-    R[0] = c;
-    R[1] = s;
-    R[2] = -s;
-    R[3] = c;
+    const R = [c, s, -s, c];
     const S = mulMat(m, R);
 
     return {R, S};
@@ -69,9 +65,10 @@ export function svd(m) {
         const t = tao > 0 ? S[1] / (tao + w) : S[1] / (tao - w);
         c = 1.0 / Math.sqrt(t * t + 1);
         s = -t * c;
-        sig = [0,0, 0,0];
-        sig[0] = c * c * S[0] - 2 * c * s * S[1] + s * s * S[3];
-        sig[3] = s * s * S[0] + 2 * c * s * S[1] + c * c * S[3];
+        sig = [
+            c * c * S[0] - 2 * c * s * S[1] + s * s * S[3], 0,
+            0, s * s * S[0] + 2 * c * s * S[1] + c * c * S[3]
+        ];
     }
     if (sig[0] < sig[3]) {
         const tmp = sig[0];
